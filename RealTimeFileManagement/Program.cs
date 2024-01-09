5bd5b14a11c711e75c5ccd86a172f6f090938166
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 15:46:07 +0500

    Final version

--- RealTimeFileManagement/Program.cs
+++ RealTimeFileManagement/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace RealTimeFileManagement;
+﻿using System;
+using System.IO;
 
-class Program
+namespace DirectoryWatcher
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the directory to watch:");
+            string directory = Console.ReadLine();
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = directory;
+
+            watcher.Filter = "*.*";
+
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine();
+            Console.WriteLine("Started monitoring on " + directory);
+            Console.WriteLine();
+            Console.WriteLine("Don't press any key to watch monitoring");
+            Console.ReadKey();
+
+            watcher.EnableRaisingEvents = false;
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"File {Path.GetFileName(e.FullPath)} was created.");
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"File {Path.GetFileName(e.FullPath)} was deleted.");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"File {Path.GetFileName(e.OldFullPath)} was renamed to {Path.GetFileName(e.FullPath)}.");
+        }
     }
 }
--- RealTimeFileManagement/obj/Debug/net8.0/RealTimeFileManagement.AssemblyInfo.cs
+++ RealTimeFileManagement/obj/Debug/net8.0/RealTimeFileManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimeFileManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c95ab387a547dc3a1bc76127bbc6dc749262c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimeFileManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimeFileManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
